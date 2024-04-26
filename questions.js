7dc40c2afd4c07667001936a7a0908e62f3f33c3
AngeloCocom1 <159877838+AngeloCocom1@users.noreply.github.com>
2024-04-26 06:36:58 -0600

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,58 @@
+//defined an array of questions along with their options and correct answers
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "C. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D. Home Text Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "E. Cascading Style Sheet",
+        options: [
+            "E. Cascading Style Sheet",
+            "F. Cute Style Sheet",
+            "G. Computer Style Sheet",
+            "H. Cadence Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP stand for?",
+        answer: "I. Hypertext Preprocessor",
+        options: [
+            "I. Hypertext Preprocessor",
+            "J. Hometext Programming",
+            "K. Hypertext Preprogramming",
+            "L. Programming Hypertext Preprocessor"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: "P. Structured Query Language",
+        options: [
+            "M. Strength Query Language",
+            "N. Stylesheet Query Language",
+            "O. Science Question Language",
+            "P. Structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "T. Extensible Markup Language",
+        options: [
+            "Q. Excellent Multiple Language",
+            "R. Explore Multiple Language",
+            "S. Extra Markup Language",
+            "T. Extensible Markup Language"
+        ]
+    }
+];
